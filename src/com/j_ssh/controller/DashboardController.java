ade5dc49bb8a8eca9e137935d56b656e66f2c23d
Jared <TheWolfBadger@gmail.com>
2022-11-16 22:35:32 -0500

    + More updates

--- src/com/j_ssh/controller/DashboardController.java
+++ src/com/j_ssh/controller/DashboardController.java
@@ -1,9 +1,12 @@
 package com.j_ssh.controller;
 
 import com.j_ssh.api.API;
+import com.j_ssh.model.DataManager;
 import com.j_ssh.view.bootstrap.BootstrapColumn;
 import com.j_ssh.view.bootstrap.BootstrapPane;
 import com.j_ssh.view.bootstrap.BootstrapRow;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class DashboardController extends BootstrapPane {
     public DashboardController() {
@@ -11,6 +14,17 @@ public class DashboardController extends BootstrapPane {
         BootstrapColumn searchCol = API.get().createColumn(null, 10);
         BootstrapColumn newSessCol = API.get().createColumn(null, 2);
         BootstrapRow iconRow = new BootstrapRow();
+        JSONArray servers = DataManager.get().getServersData();
+        for (int i = 0; i < servers.length(); i++) {
+            JSONObject serverObj = servers.getJSONObject(i);
+            String nickname = serverObj.optString("Nickname", "Server (" + (i + 1) + ")");
+            String iconURL = serverObj.optString("IconURL", "");
+            String ip = serverObj.optString("IP", "");
+            String username = serverObj.optString("Username", "");
+            String password = serverObj.optString("Password", "");
+            String privateKeyLocation = serverObj.optString("PrivateKey-Location", "");
+        }
+        // TODO Add the servers to the icon row
         this.addRow(searchRow);
         this.addRow(iconRow);
     }
